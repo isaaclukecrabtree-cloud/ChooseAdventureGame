b3d16313f5e6bc6896421c4d74200a69ee14fecb
I.Crabtree <isaaclukecrabtree@gmail.com>
2025-09-15 11:45:25 +0100

    simplified player file, removed unnecessary imports

--- player.py
+++ player.py
@@ -1,22 +1,18 @@
-from config import CharacterClass
 from lifeform import Lifeform
 
 
 class Player(Lifeform):
     def __init__(self, name="..."):
-        super().__init__(name=name,
-                         health=5,
-                         base_damage=1)
+        super().__init__(name, 5, 1)
         self.charisma = 0
         self.stealth = 0
-
         self.character_class = None
-        self.weapon = None
+        self.equipped_weapon = None
 
     def set_class(self, character_class):
         self.character_class = character_class
-        self.health = character_class.max_health
-        self.max_health = character_class.max_health
+        # Apply all class stats at once
+        self.health = self.max_health = character_class.max_health
         self.base_damage = character_class.base_damage
         self.charisma = character_class.charisma
         self.stealth = character_class.stealth
@@ -26,12 +22,12 @@ class Player(Lifeform):
         print(f"{self.name} picks up the {weapon}")
 
     def display_stats(self):
-        print(f'''Your character details:
-        
+        class_name = self.character_class.display_name if self.character_class else "None"
+        print(f"""Your character details:
         Name: {self.name}
-        Class: {self.character_class.display_name}
+        Class: {class_name}
         Health: {self.health}/{self.max_health}
         Damage: {self.base_damage}
         Stealth: {self.stealth}
         Charisma: {self.charisma}
-        ''')
+        """)
